a05ebe85b38f1ae9dd487dd423b44bcafbe90f1c
Theo MATHIEU <cdf.theo.mathieu@gmail.com>
2023-11-27 14:09:15 +0100

    création du dépot site dynamique

---
+++ index.js
@@ -0,0 +1,36 @@
+async function afficherWorks() {
+    const reponse = await fetch("http://localhost:5678/api/works")
+    const works = await reponse.json()
+    console.log(works)
+    for (let i= 0; i< works.length; i++) {
+        var fig = works[i]
+        var gallery = document.querySelector(".gallery")
+        var workellements = document.createElement("figure")
+        const images = fig.imageUrl
+        const titre = fig.title
+        workellements.innerHTML = "<img src=" + images + " alt=" + titre +"><figcaption>" + titre + "</figcaption>"
+        gallery.appendChild(workellements)
+  }
+}
+
+async function afficherObjets(categorie) {
+  const reponse = await fetch("http://localhost:5678/api/works")
+  var works = await reponse.json()
+  var objets = works.filter(function(objets){ return objets.categoryId == categorie})
+  console.log(objets)
+  for (let i= 0; i< objets.length; i++) {
+      var fig = objets[i]
+      var gallery = document.querySelector(".gallery")
+      var workellements = document.createElement("figure")
+      const images = fig.imageUrl
+      const titre = fig.title
+      workellements.innerHTML = "<img src=" + images + " alt=" + titre +"><figcaption>" + titre + "</figcaption>"
+      gallery.appendChild(workellements)
+  }
+}
+
+afficherWorks()
+// afficherObjets("1")
+
+//onclick="afficherObjets('1')"
+//onclick="afficherWorks()"
